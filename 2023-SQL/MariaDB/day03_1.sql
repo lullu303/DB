4d9e9ab497142540ee58c0331a84e5e820e893b7
jihyepark <109124273+lullu303@users.noreply.github.com>
2023-02-27 11:37:17 +0900

    Add files via upload

--- 2023-SQL/MariaDB/day03_1.sql
+++ 2023-SQL/MariaDB/day03_1.sql
@@ -7,3 +7,11 @@ select * from usertbl
 
 select concat(userID, name), concat_ws('-',userID, name, addr)
 from usertbl
+
+select format(123456.7890,4)
+
+select elt(2,'하나','둘','셋'), -- 둘
+		field('둘','하나','둘','셋'), -- 2 , 위치 리턴
+		find_in_set('둘','하나,둘,셋'), -- 2
+		instr('하나둘셋','둘'), -- 3번째에 있다(문자열이기때문)
+		locate('둘','하나둘셋') -- 3 
