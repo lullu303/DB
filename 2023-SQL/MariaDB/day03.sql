f06423ed8c2494f333b82750a0228a22885bab4b
jihyepark <109124273+lullu303@users.noreply.github.com>
2023-02-27 11:20:11 +0900

    Add files via upload

--- 2023-SQL/MariaDB/day03.sql
+++ 2023-SQL/MariaDB/day03.sql
@@ -14,3 +14,31 @@ from saltbl
 select no, sal *12+ifnull(comm,0) as annualSal
 from saltbl
 
+select nullif(100,100), nullif(200,100)
+
+select
+	case 10
+		when 1 then '일'
+		when 5 then '오'
+		when 10 then '십'
+		else '글쎄요'
+	end
+	
+	
+select * from usertbl
+
+select 
+	case height
+		when 182 then '180보다 크다'
+		when 170 then '170이다'
+		else height
+	end
+from usertbl
+
+select 
+	case sal
+		when 1000 then sal * 12 + comm
+		when 500 then sal * 12
+		else sal
+	end as annualSal
+from saltbl
